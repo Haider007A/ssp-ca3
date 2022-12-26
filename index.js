a850c305a76a464e51d99820596245ccc8eda720
Haider007A <2021384@student.cct.ie>
2022-12-26 02:02:22 +0000

    Add .gitignore and make final changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const   http = require('http'), //HTTP server
         router = express(), //Init our router
         xml2js = require('xml2js'),
         server = http.createServer(router); //Init our server
-
+        
         router.use(express.static(path.resolve(__dirname,'views')));
         router.use(express.urlencoded({extended: true}));
         router.use(express.json());
